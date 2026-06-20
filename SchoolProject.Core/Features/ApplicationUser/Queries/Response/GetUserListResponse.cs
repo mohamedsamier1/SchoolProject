6f566927f0d88c42253a59fa857b8ad1539f42bb
mohamed samir <moh2med180@outlook.com>
2026-06-20 20:45:42 +0300

    Add Change Passowrd User

--- SchoolProject.Core/Features/ApplicationUser/Queries/Response/GetUserListResponse.cs
+++ SchoolProject.Core/Features/ApplicationUser/Queries/Response/GetUserListResponse.cs
@@ -2,6 +2,7 @@
 {
     public class GetUserListResponse
     {
+        public int Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
         public string? Address { get; set; }
